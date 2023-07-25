4c46ea7211574b91688882198f48b65070e2877f
Luke Schaefer <luke.schaefer@garmin.com>
2023-07-25 17:11:27 -0500

    qupdate
    Change-Id: I050583d2418660b7adbe9a21000f79a1feae5c4f

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -2,25 +2,36 @@ import whisper
 import boto3
 import sys
 
+# s3 = boto3.resource("s3")
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
+
+print("Loading model...")
 model = whisper.load_model("large")
+print("Complete!")
 
 bucket_from = sys.argv[1]
 bucket_to = sys.argv[2]
 file_to_transcribe = sys.argv[3]
 
+print("Loading video...")
 client = boto3.client("s3")
 client.download_file(
     Bucket=bucket_from, Key=file_to_transcribe, Filename="video"
 )
+print("Complete!")
 
+print("Transcribing video...")
 result = model.transcribe("./video")
 
 txt_file = open("text.txt", "w")
 
 txt_file.write(result["text"])
+print("Complete!")
 
+print("Uploading text...")
 client.upload_file(
     Filename="text.txt",
     Bucket=bucket_to,
-    Key=file + ".transcription"
+    Key=file_to_transcribe + ".transcription"
 )
