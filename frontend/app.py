3513983c8bedfb0aab453953768f8b774acb503d
Luke Schaefer <luke.schaefer@garmin.com>
2023-08-04 11:59:27 -0500

    add some frontend logic
    Change-Id: Ie7d44eaf2da3d4a3b4bd00a9750c67d39957b313

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template, request, session, redirect, url_for  
-from functions import check_credentials, auth_navigate_to_page
+from functions import check_credentials
+
+# TESTING DATA
+subscribed_states = ["test1", "test2"]
+starting_dash_state = subscribed_states[0]
+recent_hearings = []
+hearing = {
+    "topic": "Test topic",
+    "date": "00-00-0000",
+    "length": "13:29"
+}
+recent_hearings.append(hearing)
+
+data = {
+    "starting_dash_state": starting_dash_state,
+    "recent_hearings": recent_hearings,
+    "subscribed_states": subscribed_states
+}
 
 
 app = Flask(__name__)
 app.secret_key = "aslfhc82173hasbcmao$&91lasf9*27129)84c"
 
 # Log in functionality
+@app.route("/", methods=["GET", "POST"])
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if(request.method == "POST"):
@@ -14,7 +32,7 @@ def login():
         valid_credentials = check_credentials(username, password)
         if valid_credentials:
             session["username"] = username
-            return redirect(url_for("dashboard"))
+            return redirect(url_for("dashboard", state=starting_dash_state))
         else:
             return render_template("pages/login.html", error="Invalid username or password. Please try again.")
     else:
@@ -25,14 +43,47 @@ def logout():
     session.pop('username', None)  
     return redirect(url_for('login'))  
 
+@app.route("/default_dashboard/")
+def default_dashboard():
+    return redirect(url_for("dashboard", state=starting_dash_state))
+
 @app.route("/dashboard")
-def dashboard():
-    return auth_navigate_to_page(session, "pages/portal/dashboard.html")
+@app.route("/dashboard/<state>")
+def dashboard(state=None):
+    if("username" not in session):
+        return redirect(url_for("login"))
+    username = session["username"]
+
+    if(state == None):
+        state = starting_dash_state
+
+    return render_template("pages/portal/dashboard.html", username=username, state=state, data=data)
+
+        
 
 @app.route("/profile")
 def profile():
-    return auth_navigate_to_page(session, "pages/portal/profile.html")
+    if("username" not in session):
+        return redirect(url_for("login"))
+    username = session["username"]
+    return render_template("pages/portal/profile.html", username=username)
 
 @app.route("/basic_table")
 def basic_table():
-    return auth_navigate_to_page(session, "pages/portal/basic-table.html")
+    if("username" not in session):
+        return redirect(url_for("login"))
+    username = session["username"]
+    return render_template("pages/portal/basic_table.html", username=username)
+
+@app.route("/icon_preview")
+def icon_preview():
+    if("username" not in session):
+        return redirect(url_for("login"))
+    username = session["username"]
+    return render_template("pages/portal/fontawesome.html", username=username)
+
+
+# Main Driver Function
+if __name__ == '__main__':
+    # Run the application on the local development server
+    app.run(debug=True)
--- frontend/functions.py
+++ frontend/functions.py
@@ -4,11 +4,3 @@ from flask import Flask, render_template, request, session, redirect, url_for
 def check_credentials(username, password):
     print("checking credentials")
     return True
-
-# Navigates to new pages on assumtion of logged in
-def auth_navigate_to_page(session, page_to):
-    if("username" in session):
-        username = session["username"]
-        return render_template(page_to, username=username)
-    else:
-        return redirect(url_for("login"))
